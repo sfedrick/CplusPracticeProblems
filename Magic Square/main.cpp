b1b6d7a0057be1c0139e811aa02cf6de22d4df53
sfedrick <sfedrick@haverford.edu>
2022-06-02 23:50:17 -0400

    solved magic square problem

--- Magic Square/main.cpp
+++ Magic Square/main.cpp
@@ -17,39 +17,50 @@ vector<string> split(const string &);
  * The function accepts 2D_INTEGER_ARRAY s as parameter.
  */
 int formingMagicSquare(vector<vector<int>> s) {
-   vector<vector<int>> A{{1,2,3},{4,5,6},{7,8,9}};
-   vector<vector<int>> B{{1,2,3},{4,5,6},{7,8,9}};
-   vector<vector<int>> C{{1,2,3},{4,5,6},{7,8,9}};
-   vector<vector<int>> D{{1,2,3},{4,5,6},{7,8,9}};
-   vector<vector<int>> E{{1,2,3},{4,5,6},{7,8,9}};
-   vector<vector<int>> F{{1,2,3},{4,5,6},{7,8,9}};
-   vector<vector<int>> G{{1,2,3},{4,5,6},{7,8,9}};
-   vector<vector<int>> H{{1,2,3},{4,5,6},{7,8,9}};
+   int cost=-1;
+   int newcost=-1;
+   vector<vector<int>> optimal_square{{0,0,0},{0,0,0},{0,0,0}};
+   vector<vector<int>> A{{8,1,6},{3,5,7},{4,9,2}};
+   vector<vector<int>> B{{6,1,8},{7,5,3},{2,9,4}};
+   vector<vector<int>> C{{4,9,2},{3,5,7},{8,1,6}};
+   vector<vector<int>> D{{2,9,4},{7,5,3},{6,1,8}};
+   vector<vector<int>> E{{8,3,4},{1,5,9},{6,7,2}};
+   vector<vector<int>> F{{4,3,8},{9,5,1},{2,7,6}};
+   vector<vector<int>> G{{6,7,2},{1,5,9},{8,3,4}};
+   vector<vector<int>> H{{2,7,6},{9,5,1},{4,3,8}};
    vector<vector<vector<int>>> MagicSquares{{A,B,C,D,E,F,G,H}};
-   return 1;
-
-}
+   for (vector<vector<vector<int>>>::iterator it=MagicSquares.begin();it!=MagicSquares.end();++it){
+      newcost=L1vectornorm2D(s,*it);
+      if(cost==-1){
+         cost=newcost;
+         optimal_square=*it;
+      }
+      else if(newcost<cost){
+         cost=newcost;
+         optimal_square=*it;
+      }
+   }
+   cout<<"optimal magic square"<<endl;
+   printVector2D(optimal_square);
+   return cost;}
 
 
 
 int main()
 {
    cout<<"enter magic square"<<endl;
-   vector<vector<int>> s{{1,2,3},{4,5,6},{7,8,9}};
-    vector<vector<int>> s2{{1,2,3},{4,5,6},{7,8,8}};
-   vector<int> A{4,5,3};
-   vector<int> B{2,2,3};
-   vector<int> C{};
-   C.resize(A.size());
-   C=subtractVectors(A,B);
-   C=absoluteVector(C);
-   cout<<"compute L1 2d vector norm"<<endl;
-   cout<<L1vectornorm2D(s,s2)<<endl;
+   vector<vector<int>> s{{4,9,2},{3,5,7},{8,1,5}};
+   // C.resize(A.size());
+   // C=subtractVectors(A,B);
+   // C=absoluteVector(C);
+   // cout<<"compute L1 2d vector norm"<<endl;
+   // cout<<L1vectornorm2D(s,s2)<<endl;
    
    //cout<<L1vectornorm(A,B);
   // printVector(absoluteVectors()); 
-   //int result = formingMagicSquare(s);
-
+   int result = formingMagicSquare(s);
+   cout<<"result"<<endl;
+   cout<<result<<endl;
    return 0;
 }
 
