50960c66165ceb2945d0da995b1a7f5972e0e540
root <fedrick.shaun1234@gmail.com>
2022-11-12 21:14:53 +0000

    set up for link list is working

--- Sum numbers Linked list /main.cpp
+++ Sum numbers Linked list /main.cpp
@@ -4,28 +4,32 @@ void printLink(ListNode *l1){
     int end = l1 ->next ->val;
     end = 1;
     while (l1){
-        cout<<"pointer value: "<<l1<<endl;
         cout<<l1->val<<endl;
         l1= l1->next;
     }  
-    cout<<"pointer value: "<<l1<<endl;
 }
     
 
-void array2LL( int list[]){
-    int size = *(&list + 1) - list;
-    cout<< size<<endl;
-    for (int i = 0; i<4; i++){
-        cout << "item in list"<<endl;
-       
-        cout<< list[i]<<endl;
-    }
-    list[0]=223;
+ListNode * array2LL( int* begin, int* end){
+   ListNode *l1 = new ListNode(*(end-1));
+   ListNode *current ;
+
+   int size = end-begin;
+   for (int i=2; i<size+1; i++){
+        l1 = new ListNode(*(end-i),l1);
+   }
+ 
+   return l1;
+}
+
+ListNode * sumLL( ListNode *l1, ListNode *l2){
+  return l1;
 }
 int main() {
-    ListNode *l2 = new ListNode(0);
-    ListNode *l1 = new ListNode(2,l2);
-    
+    array<int,5> a= {1,2,3,4,5};
+    array<int,5> b= {1,2,3,4,5};
+
+    ListNode *l1= array2LL( a.begin(), a.end());
     printLink(l1);
     return 0;
 }
